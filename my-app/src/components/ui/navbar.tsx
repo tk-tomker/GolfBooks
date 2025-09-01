920db6dc6b3880944ada2cf1e8e80ade2b68ff7d
TK <tckersey@icloud.com>
2025-09-01 15:21:04 +0100

    add more things

---
+++ my-app/src/components/ui/navbar.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+
+export function Navbar() {
+    return(
+        <nav className="w-full flex items-center justify-between p-4 border-b bg-white">
+            <div className="flex gap-2">
+                <Button variant="outline">Log</Button>
+                <Button variant="outline">Bookings</Button>
+                <Button variant="outline">Chats</Button>
+                <Button variant="outline">About Us</Button>
+                <Button variant="outline">Contact Us</Button>
+                <Button variant="outline">Profile</Button>
+            </div>
+        </nav>
+
+    );
+}
